3bd6bb26010722bd6306dcde2065c402610a9f6a
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-11-09 12:18:20 +0530

    Add files via upload

---
+++ Student_AA_2.py
@@ -0,0 +1,20 @@
+import turtle
+import random
+
+l1=[10,20,30,40,50]
+l2=[60,70,80,90,100,110]
+l3=[]
+colors=["red","green","black","blue","yellow","grey","cyan","orange"]
+
+def draw_circle(l,c):
+    for j in l:
+        #write the conditions here
+    
+#Call the functions here
+
+turtle.clear()
+
+for i in range(#write the condition here):
+    l3.append(#Add the elements of the list)
+    
+#Call the function
